229a2f24369fa018313ad3fa9608d42728c9d5ae
Alejandro Aguilar <aaguil10@unix2.lt.ucsc.edu>
2015-05-22 15:57:27 -0700

    Better script

--- data/cvs_manipulate.py
+++ data/cvs_manipulate.py
@@ -1,30 +1,28 @@
-print "Hello World\n"
-
-import csv
-#with open('eggs.csv', 'wb') as csvfile:
-#    spamwriter = csv.writer(csvfile, delimiter=' ',
-#                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    #spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-#    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
-#    spamwriter.writerow(['Spam2', 'Lovely Spam1', 'Wonderful Spam1'])
-
 import csv
 
+input_file = 'test_data.csv'
+output_file = 'test_output.csv'
 data = []
-with open('googletest.csv', 'rb') as csvfile:
+
+#Do your magic!
+def edit_row(list):
+        list[1] = '0'
+
+
+
+#read data
+with open(input_file, 'rb') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
      for row in spamreader:
-        #print ', '.join(row)
 	list = row[0].split(",")
-	list[1] = '0'
-	print list
-	str = ', '.join(list)
+	edit_row(list)
         data.append(list)
-	print str
 
-with open('eggs.csv', 'wb') as csvfile:
+#write data
+with open(output_file, 'wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',',
                             quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     for i in data:
     	spamwriter.writerow(i)
+
 
