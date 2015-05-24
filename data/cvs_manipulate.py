70317e990ed39c94ca98a3aeca46d66efeea6ede
Leif Myer <chatsboy@aol.com>
2015-05-24 14:06:37 -0700

    Added variable to record missing features

--- data/cvs_manipulate.py
+++ data/cvs_manipulate.py
@@ -4,24 +4,31 @@ input_file = 'test_data.csv'
 output_file = 'test_output.csv'
 data = []
 
+# Set to True if you want to record rows with missing features.
+recordMissingData = False
+
+
 
 #Do your magic!
 def edit_row(list):
-    list[1] = '0'
-    #pass
+    #list[1] = '0'
+    pass
 
 #read data
 with open(input_file, 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         list = []
+        featureMissing = False
         for item in row:
             splitItems = item.split(",")
             if splitItems[-1] == '' and len(splitItems) > 1:
+                featureMissing = True
                 del splitItems[-1]
             list.extend(splitItems)
         edit_row(list)
-        data.append(list)
+        if recordMissingData or not featureMissing:
+            data.append(list)
 
 #write data
 with open(output_file, 'wb') as csvfile:
