9acde2d50f28d367a121d4d584503c05c8582c9b
Leif Myer <chatsboy@aol.com>
2015-05-24 13:50:50 -0700

    Fixed bug with splitting.

--- data/cvs_manipulate.py
+++ data/cvs_manipulate.py
@@ -4,18 +4,23 @@ input_file = 'test_data.csv'
 output_file = 'test_output.csv'
 data = []
 
+
 #Do your magic!
 def edit_row(list):
-        list[1] = '0'
-
-
+    list[1] = '0'
+    #pass
 
 #read data
 with open(input_file, 'rb') as csvfile:
-     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-     for row in spamreader:
-	list = row[0].split(",")
-	edit_row(list)
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        list = []
+        for item in row:
+            splitItems = item.split(",")
+            if splitItems[-1] == '' and len(splitItems) > 1:
+                del splitItems[-1]
+            list.extend(splitItems)
+        edit_row(list)
         data.append(list)
 
 #write data
