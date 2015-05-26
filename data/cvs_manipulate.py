039968d492b1c99d460773cffbd94b0b1ab817a3
aaguil10 <aaguil10@ucsc.edu>
2015-05-25 20:07:53 -0700

    Modified cvs_manipulate.py and added has_no_missing_lables.arff

--- data/cvs_manipulate.py
+++ data/cvs_manipulate.py
@@ -1,7 +1,8 @@
 import csv
 import arff
+import math
 
-input_file = 'test_data.csv'
+input_file = 'goodtrain.csv'
 output_file = 'test_output.csv'
 output_arffFile = 'test_arff.arff'
 data = []
@@ -10,11 +11,25 @@ data = []
 recordMissingData = False
 
 
+def hasMissing(row):
+    for i in range(0,len(row)):
+        if(row[i] == ""):
+            return True
+    return False
 
 #Do your magic!
-def edit_row(list):
-    #list[1] = '0'
-    pass
+def edit_row(row):
+    row[-1] = row[-1]
+    #math.ceil(str(row[-1])*100)/100
+    try:
+        gpa = math.ceil(float(row[-1])*1)/1
+        row[-1] = gpa
+    except Exception:
+        gpa = 0
+        print "can't convet " + row[-1]
+    #row.remove(row[0])
+    print row[-1]
+    
 
 #read data
 with open(input_file, 'rb') as csvfile:
@@ -29,7 +44,9 @@ with open(input_file, 'rb') as csvfile:
             list.extend(splitItems)
         edit_row(list)
         featureMissing = True if '' in list else False
-        if recordMissingData or not featureMissing:
+        # if recordMissingData or not featureMissing:
+        #     data.append(list[1:]) # do not include id num at index 0
+        if (hasMissing(list) == False):
             data.append(list[1:]) # do not include id num at index 0
 
 #write data
@@ -60,7 +77,9 @@ arffdata = {
         (u'HSGPAunweighted', u'REAL'),
         (u'Firststyrunitsforgpa', u'REAL'),
         (u'Firststyeartotcumunits', u'REAL'),
-        (u'Firstyrcumgpa', u'REAL')
+        #(u'Firstyrcumgpa', [u'4.0',u'3.5', u'3.0', u'2.5', u'2.0', u'1.5', u'1.0', u'0.5', u'0.0'])
+        (u'Firstyrcumgpa', [u'4.0', u'3.0', u'2.0', u'1.0', u'0.0'])
+        #(u'Firstyrcumgpa', u'REAL')
     ],
     u'data': cutData,
     u'description': u'',
