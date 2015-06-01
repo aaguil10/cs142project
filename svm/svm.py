f3ce093199223dae2ce1301b25d54753ee76c88a
Alejandro Aguilar <aaguil10@ucsc.com>
2015-05-31 17:53:56 -0700

    Added some checks

--- svm/svm.py
+++ svm/svm.py
@@ -1,9 +1,15 @@
-from sklearn import svm
+# from sklearn import svm
 
-X = [[0, 0], [1, 1]]
-y = [0, 1]
-clf = svm.SVC()
-clf.fit(X, y)  
-SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
-gamma=0.0, kernel='rbf', max_iter=-1, probability=False, random_state=None,
-shrinking=True, tol=0.001, verbose=False)
+# X = [[0, 0], [1, 1]]
+# y = [0, 1]
+# clf = svm.SVC()
+# clf.fit(X, y)  
+# SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+# gamma=0.0, kernel='rbf', max_iter=-1, probability=False, random_state=None,
+# shrinking=True, tol=0.001, verbose=False)
+
+try:
+    import scipy
+    print "Yea!"
+except ImportError:
+    print "scipy is not installed"
