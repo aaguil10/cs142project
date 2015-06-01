14de8c8c75bfad80615936e2ccf57c298cb2072b
Leif Myer <chatsboy@aol.com>
2015-05-31 18:27:26 -0700

    ARFF with means and modes

---
+++ boosting/testClassificationModel.py
@@ -0,0 +1,28 @@
+import arff
+import sys
+from sklearn import linear_model
+
+# password: pythoncryeverytim
+# 'sha1:435320dcd938:cc03b9128b80c82b465481be7106ed0a6927e127'
+
+gender = {'Male' : 0, 'Female' : 1, 'Unknown' : 2}
+language = {'English' : 0, 'EnglishandAnother' : 1, 'Another' : 2}
+
+filename = sys.argv[1]
+
+examples = []
+labels = []
+
+with open(filename, 'rb') as af:
+	arffFile = arff.load(af)
+	attributes = arffFile['attributes']
+	data = arffFile['data']
+	for row in data:
+		row[0] = gender[row[0]] # index of gender
+		row[12] = language[row[12]] # index of language
+		examples.append(row[0:-1])
+		labels.append(row[-1])
+
+clf = linear_model.Perceptron()
+print clf.fit(examples, labels)
+print clf.coef_
